09ed6ff87109c47df11a972842489a8ea26b4093
pauls <pauls@coin.nl>
2019-09-04 12:56:51 +0200

    bumped to version 0.1.5

--- common-sdk/coin/sdk/common/client/RestApiClient.php
+++ common-sdk/coin/sdk/common/client/RestApiClient.php
@@ -9,7 +9,7 @@ abstract class RestApiClient
 {
     public static function getFullVersion() {
         // NOTE: automatically updated by pre_tag_command
-        return 'coin-sdk-php-0.1.4';
+        return 'coin-sdk-php-0.1.5';
     }
 
     protected $hmacSecret;

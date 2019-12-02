f6e480b6fee6f50061676e95f725c79db7807c4f
bart.vanmunster <b.vanmunster@coin.nl>
2019-12-02 12:37:57 +0100

    bumped to version 0.1.8

--- common-sdk/coin/sdk/common/client/RestApiClient.php
+++ common-sdk/coin/sdk/common/client/RestApiClient.php
@@ -9,7 +9,7 @@ abstract class RestApiClient
 {
     public static function getFullVersion() {
         // NOTE: automatically updated by pre_tag_command
-        return 'coin-sdk-php-0.1.7';
+        return 'coin-sdk-php-0.1.8';
     }
 
     protected $hmacSecret;
